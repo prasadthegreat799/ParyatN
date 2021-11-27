eaa3f4cb693b8cb2be8e8ac2a0c5eaee638d1ef3
Aravind Jagarlamudi <o161800@rguktrkv.ac.in>
2021-11-27 17:45:40 +0530

    Explore UI Edited

--- app/src/main/java/com/nappdeveloper/paryatn/MainActivity.java
+++ app/src/main/java/com/nappdeveloper/paryatn/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,6 +20,7 @@ import com.nappdeveloper.paryatn.Fragments.toursFragment;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    RelativeLayout relativeLayout_main;
     View v1, v2, v3, v4;
 
     @Override
@@ -30,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         v2 = findViewById(R.id.view2);
         v3 = findViewById(R.id.view3);
         v4 = findViewById(R.id.view4);
+
+        relativeLayout_main = (RelativeLayout) findViewById(R.id.main_layout);
 
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.BottomNavigationView);
         bottomNavigationView.setBackground(null);
@@ -54,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
                             v2.setBackgroundColor(getResources().getColor(R.color.white));
                             v3.setBackgroundColor(getResources().getColor(R.color.white));
                             v4.setBackgroundColor(getResources().getColor(R.color.white));
+                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.browser_actions_bg_grey));
                             break;
                         case R.id.exploreMenu:
                             fragment = new exploreFragment();
@@ -61,6 +66,7 @@ public class MainActivity extends AppCompatActivity {
                             v1.setBackgroundColor(getResources().getColor(R.color.white));
                             v3.setBackgroundColor(getResources().getColor(R.color.white));
                             v4.setBackgroundColor(getResources().getColor(R.color.white));
+                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.purple_200));
                             break;
                         case R.id.toursMenu:
                             fragment = new toursFragment();
@@ -68,6 +74,7 @@ public class MainActivity extends AppCompatActivity {
                             v1.setBackgroundColor(getResources().getColor(R.color.white));
                             v2.setBackgroundColor(getResources().getColor(R.color.white));
                             v4.setBackgroundColor(getResources().getColor(R.color.white));
+                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.browser_actions_bg_grey));
                             break;
                         case R.id.profileMenu:
                             fragment = new profileFragment();
@@ -75,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
                             v1.setBackgroundColor(getResources().getColor(R.color.white));
                             v2.setBackgroundColor(getResources().getColor(R.color.white));
                             v3.setBackgroundColor(getResources().getColor(R.color.white));
+                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.white));
                             break;
 
                         default:
