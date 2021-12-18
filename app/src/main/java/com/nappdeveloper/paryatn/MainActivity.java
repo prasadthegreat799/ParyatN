f02343b6d6e0fe8c2dd9247f0e7cb1116ea63230
Neela Prasad <prasadthegreat799@gmail.com>
2021-12-18 20:41:27 +0530

    modified bottom navigation

--- app/src/main/java/com/nappdeveloper/paryatn/MainActivity.java
+++ app/src/main/java/com/nappdeveloper/paryatn/MainActivity.java
@@ -33,10 +33,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*
         v1 = findViewById(R.id.view1);
         v2 = findViewById(R.id.view2);
         v3 = findViewById(R.id.view3);
-        v4 = findViewById(R.id.view4);
+        v4 = findViewById(R.id.view4); */
 
         relativeLayout_main = (RelativeLayout) findViewById(R.id.main_layout);
 
@@ -59,35 +60,37 @@ public class MainActivity extends AppCompatActivity {
 
                         case R.id.homeMenu:
                             fragment = new homeFragment();
-                            v1.setBackgroundColor(getResources().getColor(R.color.purple_500));
+                           /* v1.setBackgroundColor(getResources().getColor(R.color.purple_500));
                             v2.setBackgroundColor(getResources().getColor(R.color.white));
                             v3.setBackgroundColor(getResources().getColor(R.color.white));
                             v4.setBackgroundColor(getResources().getColor(R.color.white));
-                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.white));
+                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.white)); */
                             break;
                         case R.id.exploreMenu:
                             fragment = new exploreFragment();
-                            v3.setBackgroundColor(getResources().getColor(R.color.purple_500));
+                           /* v3.setBackgroundColor(getResources().getColor(R.color.purple_500));
                             v1.setBackgroundColor(getResources().getColor(R.color.white));
                             v2.setBackgroundColor(getResources().getColor(R.color.white));
                             v4.setBackgroundColor(getResources().getColor(R.color.white));
-                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.purple_200));
+                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.purple_200)); */
                             break;
                         case R.id.toursMenu:
                             fragment = new toursFragment();
-                            v2.setBackgroundColor(getResources().getColor(R.color.purple_500));
+
+                           /* v2.setBackgroundColor(getResources().getColor(R.color.purple_500));
                             v1.setBackgroundColor(getResources().getColor(R.color.white));
                             v3.setBackgroundColor(getResources().getColor(R.color.white));
                             v4.setBackgroundColor(getResources().getColor(R.color.white));
-                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.browser_actions_bg_grey));
+                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.browser_actions_bg_grey)); */
                             break;
                         case R.id.profileMenu:
                             fragment = new profileFragment();
-                            v4.setBackgroundColor(getResources().getColor(R.color.purple_500));
+
+                          /*  v4.setBackgroundColor(getResources().getColor(R.color.purple_500));
                             v1.setBackgroundColor(getResources().getColor(R.color.white));
                             v2.setBackgroundColor(getResources().getColor(R.color.white));
                             v3.setBackgroundColor(getResources().getColor(R.color.white));
-                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.white));
+                            relativeLayout_main.setBackgroundColor(getResources().getColor(R.color.white)); */
                             break;
 
                         default:
